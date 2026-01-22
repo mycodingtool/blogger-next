cc3c6c2a6280922e8b91a88779c41b03c1271efd
mycodingtool <debchunari35@gmail.com>
2026-01-22 09:55:54 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "My Blogger Blog",
+  description: "Blog powered by Blogger API and Next.js"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
 
